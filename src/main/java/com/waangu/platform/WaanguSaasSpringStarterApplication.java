378f058ecfca74c407c937ba102182f2dc450ae7
ibrahim274651 <ikwizera@asystresources.com>
2026-02-12 15:18:06 +0200

    init

---
+++ src/main/java/com/waangu/platform/WaanguSaasSpringStarterApplication.java
@@ -0,0 +1,13 @@
+package com.waangu.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaanguSaasSpringStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaanguSaasSpringStarterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/waangu/platform/WaanguSaasSpringStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.waangu.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaanguSaasSpringStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
